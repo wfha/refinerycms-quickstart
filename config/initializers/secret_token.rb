e17877e8739786c38f410dcd343ed6c1fbc79a1d
Hiro Asari <asari.ruby@gmail.com>
2012-10-05 10:40:22 -0400

    Refinery-generated code with PostgreSQL as database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = '2dbb137ab4f45c9d96d4af90e009562735b795480e8de029243347747b8ffb30e180b79f64a93b4976f33165ba151bb626ff917e3920a17b5a86858c4a5954fc'
